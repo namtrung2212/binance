7245540b35124dc2f2ad3505953c1ea70847ee3b
namtrung2212 <namtrung2212@gmail.com>
2017-12-16 15:07:01 +0700

    merge in 1 index.js

--- LTC-BTC.js
+++
@@ -1,6 +0,0 @@
-
-const AutoBot = require("./AutoBot");
-
-var bot1 = new AutoBot("LTC", "BTC", 0.5, "5m", 0.5);
-bot1.initRedis("6379", "localhost");
-bot1.start();
---
+++ index.js
@@ -0,0 +1,39 @@
+
+const AutoBot = require("./AutoBot");
+
+var bots = [
+    { trade: "BTC", base: "USDT" },
+    { trade: "ETH", base: "USDT" },
+    { trade: "LTC", base: "USDT" },
+    { trade: "XRP", base: "USDT" },
+
+    { trade: "ETH", base: "BTC" },
+    { trade: "LTC", base: "BTC" },
+    { trade: "XRP", base: "BTC" },
+
+    { trade: "LTC", base: "ETH" },
+    { trade: "XRP", base: "ETH" },
+
+    { trade: "XRP", base: "LTC" }
+];
+
+var root = {};
+
+root.start = function (bots) {
+
+    setTimeout(this.timerHandler, 1000 * 2, bots, 0, this);
+};
+
+root.timerHandler = async function (bots, current, root) {
+
+    if (current < 0 || current >= bots.length)
+        return;
+
+    var bot = new AutoBot(bots[current].trade, bots[current].base, 1, "5m", 0.5);
+    bot.initRedis("6379", "localhost");
+    bot.start();
+
+    setTimeout(root.timerHandler, 1000 * 10, bots, current + 1, root);
+};
+
+root.start(bots);
