b8f89bee401d94e0f829e417b34b13ea9c3259b0
namtrung2212 <namtrung2212@gmail.com>
2017-12-16 23:44:40 +0700

    no message

--- REST.js
+++ REST.js
@@ -13,13 +13,9 @@ var API = new BinanceAPI("XRP", "BTC");
 var caching = RedisClient.createClient("6379", "localhost");
 caching.flushall();
 
-const app = express();
-
-app.listen(57575);
-
-app.set('view engine', 'ejs');
-
-app.get('/balance', function (req, res) {
+const RESTServer = express();
+RESTServer.listen(57575);
+RESTServer.get('/balance', function (req, res) {
 
     var currency = "USDT";
     if (req.query.currency)
@@ -33,7 +29,10 @@ app.get('/balance', function (req, res) {
 
 });
 
-app.get('/webbalance', function (req, res) {
+const WebServer = express();
+WebServer.listen(8080);
+WebServer.set('view engine', 'ejs');
+WebServer.get('/balance', function (req, res) {
 
     var currency = "USDT";
     if (req.query.currency)
