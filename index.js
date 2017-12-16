57679f23a1e59e6b0ac97a5c39bc8779cb21dfeb
namtrung2212 <namtrung2212@gmail.com>
2017-12-16 16:59:32 +0700

    no message

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ var bots = [
     { trade: "BTC", base: "USDT" },
     { trade: "ETH", base: "USDT" },
     { trade: "LTC", base: "USDT" },
+    { trade: "BNB", base: "USDT" },
     //  { trade: "XRP", base: "USDT" },
 
     { trade: "ETH", base: "BTC" },
     { trade: "LTC", base: "BTC" },
     { trade: "XRP", base: "BTC" },
+    { trade: "ETC", base: "BTC" },
+    { trade: "BNB", base: "BTC" },
 
     { trade: "LTC", base: "ETH" },
-    { trade: "XRP", base: "ETH" }
+    { trade: "XRP", base: "ETH" },
+    { trade: "ETC", base: "ETH" },
+    { trade: "BNB", base: "ETH" }
 
     //  { trade: "XRP", base: "LTC" }
 ];
@@ -29,7 +34,7 @@ root.timerHandler = async function (bots, current, root) {
     if (current < 0 || current >= bots.length)
         return;
 
-    var bot = new AutoBot(bots[current].trade, bots[current].base, "5m", 30);
+    var bot = new AutoBot(bots[current].trade, bots[current].base, "1m", 30);
     bot.initRedis("6379", "localhost");
     bot.start();
 
